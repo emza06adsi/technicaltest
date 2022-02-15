df1e5929ccdf5ed4e066223d77dbf9bdd3617ff9
Karlo A. Lopez <karlo.lopez@syngenta.com>
2022-02-15 11:33:19 -0600

    feat: main navigation file

---
+++ src/navigation/main-navigation.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+import LoginScreen from 'screens/Login'
+
+const Stack = createStackNavigator()
+
+const GlobalStackNavigator = () => (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name='LoginScreen' component={LoginScreen} />
+    </Stack.Navigator>
+)
+  
+export default GlobalStackNavigator
+  

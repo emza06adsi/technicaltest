23b5c851cff12a56ae8d90a842459d6bd5e04fe3
Karlo A. Lopez <karlo.lopez@syngenta.com>
2022-02-15 11:33:32 -0600

    feat: login screen bone

---
+++ src/screens/Login/index.tsx
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from 'react';
+import styles from './styles';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function LoginScreen() {
+  return (<SafeAreaView style={styles.container}>
+  </SafeAreaView>)
+}
---
+++ src/screens/Login/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+import { Colors } from 'utils/colors';
+
+const styles = StyleSheet.create({
+    container:{
+        paddingTop: 25,
+        marginHorizontal: 15,
+        backgroundColor: Colors.PrimaryBackground
+    },
+})
+
+export default styles;

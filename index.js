7d5486881ce3ddac129e578bb7f316684e26f19f
Karlo A. Lopez <karlo.lopez@syngenta.com>
2022-02-15 11:33:04 -0600

    feat: new index.js file

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 /**
  * @format
  */
-
+import React from 'react';
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './App.tsx';
 import {name as appName} from './app.json';
+import {Provider} from 'react-redux';
+import store from './src/redux/store';
 
-AppRegistry.registerComponent(appName, () => App);
+const RnRedux = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+AppRegistry.registerComponent(appName, () => RnRedux);

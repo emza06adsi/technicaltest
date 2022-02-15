4c501e57f30adb54c20ae899f0ba3f5be228cde2
Karlo A. Lopez <karlo.lopez@syngenta.com>
2022-02-15 12:00:36 -0600

    feat: final navigation file

--- src/navigation/main-navigation.tsx
+++ src/navigation/main-navigation.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
-import LoginScreen from 'screens/Login'
+import LoginScreen from '../screens/Login'
 
 const Stack = createStackNavigator()
 
